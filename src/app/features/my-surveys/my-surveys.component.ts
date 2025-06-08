0f200a4dee5fead99ee1a84fadd6d08f3ec02f84
Danylo Zherdiev <zerdevdanil430@gmail.com>
2025-06-08 19:02:02 +0300

    create button survey results and copy link

--- src/app/features/my-surveys/my-surveys.component.ts
+++ src/app/features/my-surveys/my-surveys.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {  SurveyService } from 'src/app/core/services/survey.service';
 import { Router } from '@angular/router';
 import { Survey } from 'src/app/core/models/survey.model';
+import { error } from 'jquery';
 @Component({
   selector: 'app-my-surveys',
   templateUrl: './my-surveys.component.html',
@@ -14,11 +15,29 @@ export class MySurveysComponent implements OnInit{
   viewDetails(id:number){
     this.router.navigate(['/survey-details',id])
   }
+
+  viewResults(id:number):void{
+    this.router.navigate(['/survey-results',id]);
+  }
   
   ngOnInit(): void {
       this.surveyService.getMySurveys().subscribe({
         next: (data) => this.surveys=data,
         error: (err) => console.error('Помилка завантаження опитувань',err)
       });
+  }
+
+
+  copyLink(id:number):void{
+    const url = `${window.location.origin}/survey/${id}`;
+    navigator.clipboard.writeText(url)
+    .then(()=>{
+      alert('Посилання скопійовано в буфер обміну:\n'+ url);
+    })
+    .catch(err => {
+      console.error('Не вдалося скопіювати посилання',err);
+      alert('Не вдалося скопіювати посилання');
+    })
+    
   }
 }
